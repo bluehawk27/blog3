68b2d0fbdb968ecae1e82d99a561479f1ba64019
bluehawk27 <avillacorta27@gmail.com>
2015-06-18 15:14:22 -0700

    fix contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,4 @@
 class ContactsController < ApplicationController
-
   def new
     @contact = Contact.new
   end
@@ -11,8 +10,7 @@ class ContactsController < ApplicationController
       flash.now[:error] = nil
     else
       flash.now[:error] = 'Cannot send message.'
-      render :create
+      render :new
     end
   end
 end
-
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,13 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\\-])@([\w\-]\.)([\w]{2,})\z/i
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
 
   def headers
     {
       :subject => "Contact Form",
-      :to => "avillacorta27@gmail.com",
+      :to => "avillacorta27@gmail.com}",
       :from => %("#{name}" <#{email}>)
     }
   end
